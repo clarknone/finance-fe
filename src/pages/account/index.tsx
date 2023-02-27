dfd55e650a9a05abf4b8df5797d7ef0a862ce49a
carla <carla@mail.com>
2023-02-27 22:09:58 +0100

    page setup

---
+++ src/pages/account/index.tsx
@@ -0,0 +1,10 @@
+import UserLayout from '@/layout/user'
+import { Box, Typography } from '@mui/material'
+
+export default function Dashboard() {
+  return (
+    <UserLayout>
+      <Typography> Dashboard Page </Typography>
+    </UserLayout>
+  )
+}
---
+++ src/pages/signup.tsx
@@ -0,0 +1,9 @@
+import { Box, Typography } from '@mui/material'
+
+export default function SignUp() {
+  return (
+    <Box>
+      <Typography> SignUp Page </Typography>
+    </Box>
+  )
+}
