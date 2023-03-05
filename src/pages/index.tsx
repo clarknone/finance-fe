9c9a90e93cd9fe670536c70c5b302e1a1841ad00
carla <carla@mail.com>
2023-03-05 23:03:14 +0100

    ui: auth ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,65 @@
-import { Box, Typography } from '@mui/material'
+import LoadingButton from "@/component/general/loadingButton";
+import CustomSnackBar from "@/component/general/snackbar";
+import { useAuthContext } from "@/context/auth";
+import { ILoginForm } from "@/interfaces/auth/form";
+import { Button, Card,  Stack, TextField, Typography } from "@mui/material";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { FormEvent, useState } from "react";
 
-export default function Home() {
+export default function AuthPage() {
+  const router = useRouter();
+  const { login } = useAuthContext();
+  const [snackbar, setSnackbar] = useState<{ error?: string; success?: string }>({});
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data: ILoginForm = { email: e.currentTarget["email"].value, password: e.currentTarget["password"].value };
+    setSnackbar({});
+    setLoading(true);
+    login &&
+      login(data)
+        .then(() => {
+          setSnackbar(() => ({ success: "Login sucessful" }));
+          router.push("/user");
+        })
+        .catch((e) => {
+          console.log({ m: e.message });
+          setSnackbar(() => ({ error: e.message }));
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+  };
   return (
-    <Box>
-      <Typography> Home Page </Typography>
-    </Box>
-  )
+    <Stack justifyContent={"center"} alignItems="center" sx={{ minHeight: "100vh" }}>
+      <Card elevation={0}>
+        <Stack p={"1em"} gap={"1em"} justifyContent="center">
+          <Typography>
+            {" "}
+            Login to Continue or{" "}
+            <Link href={"/signup"}>
+              <Button> Sign Up </Button>
+            </Link>
+          </Typography>
+          <form onSubmit={handleLogin}>
+            <Stack gap={"2em"}>
+              <TextField label="Email" name="email" type={"email"} />
+              <TextField label="Password" name="password" type={"password"} />
+              <LoadingButton variant="contained" loading={loading} type="submit">
+                Login
+              </LoadingButton>
+            </Stack>
+          </form>
+        </Stack>
+      </Card>
+
+      <CustomSnackBar
+        display={Boolean(snackbar.error || snackbar.success)}
+        type={snackbar.success ? "success" : "error"}
+        message={snackbar.error || snackbar.success}
+      />
+    </Stack>
+  );
 }
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,9 +1,71 @@
-import { Box, Typography } from '@mui/material'
+import LoadingButton from "@/component/general/loadingButton";
+import CustomSnackBar from "@/component/general/snackbar";
+import { useAuthContext } from "@/context/auth";
+import { ISignupForm } from "@/interfaces/auth/form";
+import { Box, Button, Card, CardContent, Stack, TextField, Typography } from "@mui/material";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { FormEvent, useState } from "react";
 
-export default function SignUp() {
+export default function AuthPage() {
+  const router = useRouter();
+  const { signUp } = useAuthContext();
+  const [snackbar, setSnackbar] = useState<{ error?: string; success?: string }>({});
+  const [loading, setLoading] = useState(false);
+
+  const handleSignup = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data: ISignupForm = {
+      email: e.currentTarget["email"].value,
+      password: e.currentTarget["password"].value,
+      fullname: e.currentTarget["fullname"].value,
+    };
+    setSnackbar({});
+    setLoading(true);
+    signUp &&
+      signUp(data)
+        .then(() => {
+          setSnackbar(() => ({ success: "Login sucessful" }));
+          router.push("/user");
+        })
+        .catch((e) => {
+          setSnackbar(() => ({ error: e.message }));
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+  };
   return (
-    <Box>
-      <Typography> SignUp Page </Typography>
-    </Box>
-  )
+    <Stack justifyContent={"center"} alignItems="center" sx={{ minHeight: "100vh" }}>
+      <Card elevation={0}>
+        <CardContent>
+          <Typography>
+            Create an account or{" "}
+            <Link href={"/"}>
+              <Button> Sign In </Button>
+            </Link>
+          </Typography>
+          <form onSubmit={handleSignup}>
+            <Stack gap={"2em"}>
+              <TextField label="Full Name" name="fullname" required />
+              <TextField label="Phone Number" name="phone" required />
+              <TextField label="Email" name="email" type={"email"} required />
+              <TextField label="Password" name="password" type={"password"} required />
+              <Box>
+                <LoadingButton loading={loading} type="submit">
+                  SignUp
+                </LoadingButton>
+              </Box>
+            </Stack>
+          </form>
+        </CardContent>
+      </Card>
+
+      <CustomSnackBar
+        display={Boolean(snackbar.error || snackbar.success)}
+        type={snackbar.success ? "success" : "error"}
+        message={snackbar.error || snackbar.success}
+      />
+    </Stack>
+  );
 }
